9d99c0d781a5e5f096c95611607945af5945406e
Khes Adderley <khesadderley@khess-air-1.home>
2023-05-29 12:15:09 +0200

    additional components included

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,18 +1,15 @@
 import React, { Component } from "react";
 class Name extends Component {
-//   state = { like: false}; <-- this was moved up to the parent, Character to raise up the state (ie. callback)
+  //   state = { like: false}; <-- this was moved up to the parent, Character to raise up the state (ie. callback)
 
   render() {
-
-    const { like, character } = this.props; 
+    const { like, character } = this.props;
 
     return (
       <div>
         <h1>{character}</h1>
-        <button
-          onClick={this.props.onLikeToggle}
-        >
-          {like ? 'Like' : 'Not Liked'}
+        <button onClick={this.props.onLikeToggle}>
+          {like ? "Like" : "Not Liked"}
         </button>
       </div>
     );
